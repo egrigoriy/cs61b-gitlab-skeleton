6567310c55f44510ccb01cf09a9eee54d4f410f6
Grigoriy <egrigoriy@gmail.com>
2024-08-01 13:17:33 +0300

    Lab 1 added

---
+++ lab1/Collatz.java
@@ -0,0 +1,33 @@
+/** Class that prints the Collatz sequence starting from a given number.
+ *  @author YOUR NAME HERE
+ */
+public class Collatz {
+
+    /**
+     * Returns for given integer n, the next number in Collatz sequence, i.e.
+     * n/2 if n is even
+     * 3n + 1 if n is odd
+     * @param an integer n
+     * @return the next number in Collatz sequence
+     */
+    public static int nextNumber(int n) {
+        if (n % 2 == 0) {
+            return n / 2;
+        }
+        if (n % 2 != 0) {
+            return 3 * n + 1;
+        }
+        return 1;
+    }
+
+    public static void main(String[] args) {
+        int n = 5;
+        System.out.print(n + " ");
+        while (n != 1) {
+            n = nextNumber(n);
+            System.out.print(n + " ");
+        }
+        System.out.println();
+    }
+}
+
